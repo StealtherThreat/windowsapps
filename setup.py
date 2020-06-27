a180c5fbe5f4c9849e1b3f1206974709122c9f9b
Stealtherthreat <princetushargoyal@gmail.com>
2020-06-27 20:46:49 +0530

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="windowsapps",
+    version="0.0.1",
+    author="Tushar Goyal",
+    author_email="StealtherThreat@outlook.com",
+    description="Windows Apps for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StealtherThreat/windowsapps",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ],
+    python_requires='>=3.6',
+)
